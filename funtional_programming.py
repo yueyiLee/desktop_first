73b5f6eb31fdefcd879cb5196ea2f660de2a1351
Eric Lee <jllisiwei1993@163.com>
2017-07-26 21:19:12 +0800

    the rest of that .py file

--- funtional_programming.py
+++ funtional_programming.py
@@ -128,3 +128,9 @@ def now():
     print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 now()
 
+# 偏函数？？？
+# functools.partial的作用就是，把一个函数的某些参数给固定住（也就是设置默认值），返回一个新的函数，调用这个新函数会更简单。
+int2 = functools.partial(int,base=2)
+print(int2('345'))
+
+print('change in github!')
